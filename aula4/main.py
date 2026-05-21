a5b7fda2cbbe4d3135b2cd4b90335f8d66476ae0
Thomas Andrew <thommy.andrew21@gmail.com>
2026-05-21 07:59:43 -0300

    aula 4 - fix errors

--- aula4/main.py
+++ aula4/main.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@aoo.route('/')
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@aoo.route('/page2')
+@app.route('/page2')
 def page2():
     return render_template('page2.html')
 
-@aoo.route('/page3')
+@app.route('/page3')
 def page3():
     return render_template('page3.html')
 
-@aoo.route('/page4')
+@app.route('/page4')
 def page4():
     return render_template('page4.html')
 
